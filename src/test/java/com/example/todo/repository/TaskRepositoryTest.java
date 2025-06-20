d62e421bddc577c3b4e1aa8c050c6f8b12f112d9
kadowakiyuya <yuya.kadowaki@avantcorp.com>
2025-06-20 11:29:19 +0900

    コード検証

--- src/test/java/com/example/todo/repository/TaskRepositoryTest.java
+++ src/test/java/com/example/todo/repository/TaskRepositoryTest.java
@@ -4,12 +4,13 @@ import com.example.todo.model.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 public class TaskRepositoryTest {
 
     @Autowired
